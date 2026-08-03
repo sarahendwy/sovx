c90779a907276eed587a9a06ab27abdfd34e187d
sarahendwy <111707392+sarahendwy@users.noreply.github.com>
2026-08-04 00:17:02 +0300

    feat: enhance order form with Arabic labels and improved styling

--- EveAccessories/orders/forms.py
+++ EveAccessories/orders/forms.py
@@ -11,6 +11,74 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         exclude = ['user', 'status', 'order_total', 'created_at', 'updated_at', 'products', 'quanitites', 'payment_account', 'payment_method', 'payment_proof', 'delivered_at']
+        labels = {
+           "name": "الاسم",
+           "store_name": "اسم المحل",
+           "email": "البريد الإلكتروني",
+           "phone": "رقم التليفون",
+           "governorate": "المحافظة",
+           "city": "المدينة",
+           "address": "العنوان بالتفصيل",
+           "message": "رسالتك",
+        }
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "class": "form-control order-form-control",
+                    "placeholder": "اكتب اسمك",
+                    "dir": "rtl",
+                }
+            ),
+            "store_name": forms.TextInput(
+                attrs={
+                    "class": "form-control order-form-control",
+                    "placeholder": "اكتب اسم المحل",
+                    "dir": "rtl",
+                }
+            ),
+            "email": forms.EmailInput(
+                attrs={
+                    "class": "form-control order-form-control",
+                    "placeholder": "اكتب البريد الإلكتروني",
+                    "dir": "rtl",
+                }
+            ),
+            "phone": forms.TextInput(
+                attrs={
+                    "class": "form-control order-form-control",
+                    "placeholder": "000-000-000-00",
+                    "dir": "ltr",
+                }
+            ),
+            "governorate": forms.Select(
+                attrs={
+                    "class": "form-select order-form-control",
+                    "dir": "rtl",
+                }
+            ),
+            "city": forms.Select(
+                attrs={
+                    "class": "form-select order-form-control",
+                    "dir": "rtl",
+                }
+            ),
+            "address": forms.TextInput(
+                attrs={
+                    "class": "form-control order-form-control",
+                    "placeholder": "اكتب العنوان بالتفصيل",
+                    "dir": "rtl",
+                }
+            ),
+            "message": forms.Textarea(
+                attrs={
+                    "class": "form-control order-form-control",
+                    "placeholder": "نحن هنا لنسمع رأيكم",
+                    "rows": 4,
+                    "dir": "rtl",
+                }
+            ),
+        }
+
 
     def clean_quanitites(self):
         quanitites = self.cleaned_data.get('quanitites')
