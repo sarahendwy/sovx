3c9120b77cbeb361be8418ffba494a72ecf9847e
Moustafa Ascoura <moustafa.a@blackrockts.com>
2026-09-13 23:11:03 +0300

    feat: Update URL patterns and templates for improved navigation and accessibility

--- SOVX/orders/urls.py
+++ SOVX/orders/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('create', CreateOrder.as_view(), name='create_order'),
     path('sell', SellWithUs.as_view(), name='sell_with_us'),
-    path('contact', ContactUs.as_view(), name='contact_us'),
+    path('contact', ContactUs.as_view(), name='contact'),
     path('thank-you', ThankYou.as_view(), name='thank_you'),
 ]
--- SOVX/products/urls.py
+++ SOVX/products/urls.py
@@ -3,6 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('articles', index, name='articles'),
+    path('about', index, name='about'),
     path('products', ProductListView.as_view(), name='products'),
     path('product/<int:pk>', ProductView.as_view(), name='product'),
 ]
+    
